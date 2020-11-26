28cf63c60d8e48f435fdca46d23a13aadcf14aa0
Alexey Shkurpela <alex.shkurpela@gmail.com>
2020-11-26 14:27:00 +0200

    fix example

--- example_exchange.php
+++ example_exchange.php
@@ -1,26 +1,30 @@
 <?php
 use transcoinme\api\Exchange as Exchange;
 
+$apiUrl = ''; // Your API URL
+$apiKey = ''; // Your API Key
+$partnerId = ''; // Your partner id
+
 //Include the Exchange class
 require_once __DIR__ .'/exchange.php';
 
 //Create Exchange object
 //API URL and Your API Key you may find on the settings page on our website
-$exch = new Exchange(<Your API URL>,<Your API Key>);
+$exch = new Exchange($apiUrl, $apiKey);
 
 //First, we request data for calculating the transaction
 $exch->getCalcData(array(
-	'partner_id'    => <Your ID>, //you may find on the settings page on our website
+	'partner_id'    => $partnerId, //you may find on the settings page on our website
 ));
 // Request result will store in result property of the Exchange object (will be overwriten upon repeated request)
 print_r($exch->result); 
 echo '<br><br>';
 
 $exch->process(array(
-    'partner_id'    => <Your ID>, //you may find on the settings page on our website
-    'wallet' 		=> <Your wallet>, // be very careful and attentive - erroneous data 
+    'partner_id'    => $partnerId, //you may find on the settings page on our website
+    'wallet' 		=> 'Your wallet', // be very careful and attentive - erroneous data 
 									  //can lead to the access to your financial transactions by others
-    'email'      	=> <Your email>,
+    'email'      	=> 'user@email.com', // user email
     'method' 		=> 2, //method ID (you may get it from request getCalcData)
     'from'  		=> 2, //currency ID (you may get it from request getCalcData)
     'to'   			=> 3, //cryptocurrency ID (you may get it from request getCalcData)
@@ -38,14 +42,14 @@ echo '<br><br>';
 
 $res = json_decode($exch->result, true);
 $exch->exchange(array( // After request, result will be overwritten
-    'partner_id'    => <Your ID>, //you may find on the settings page on our website
+    'partner_id'    => $partnerId, //you may find on the settings page on our website
     'exchange_id'    => $res['id'], //Transaction ID.
     ));
 print_r($exch->result); 
 echo '<br><br>';
 
 $exch->getCalcComissions(array(
-	'partner_id'    => <Your ID>, //you may find on the settings page on our website
+	'partner_id'    => $partnerId, //you may find on the settings page on our website
     'method' 		=> 2, //method ID (you may get it from request getCalcData)
     'from'  		=> 2, //currency ID (you may get it from request getCalcData)
     'to'   			=> 3, //cryptocurrency ID (you may get it from request getCalcData)
